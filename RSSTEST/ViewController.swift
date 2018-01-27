0398fc4360cfd5f4045fab7328829e0076380da3
DaichiSaito <h1a_ble@yahoo.co.jp>
2018-01-27 21:26:03 +0900

    firstcommit

--- RSSTEST/ViewController.swift
+++ RSSTEST/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import Alamofire
+class ViewController: UIViewController, XMLParserDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        fetchRSS()
+        
+//        loadXML()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,104 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func fetchRSS() {
+        let parameters: Parameters = ["q": "select * from rss where url = 'http://soccerlture.com/feed/'", "format": "json"]
+        Alamofire.request("https://query.yahooapis.com/v1/public/yql", parameters: parameters).responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+//            print(response.result.value)
+            if let json = response.result.value {
+//                print("JSON: \(json)") // serialized json response
+                
+//                print(json["query"])
+//                print(json["query"]["results"]["item"])
+//                let data = json.data(using: .utf8)!
+//                let data = json
+//                do {
+//                    let dic = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] // ["fuga": "aiueo", "user": ["name": "tenten0213", "age": 30], "foo": 999]
+//                    dic?["fuga"] // "aiueo"
+//                    let user = dic?["user"] as? [String: Any] // ["name": "tenten0213", "age": 30]
+//                    user?["name"] as? String // "tenten0213"
+//                    user?["age"] as? Int // 30
+//                } catch {
+//                    print(error.localizedDescription)
+//                }
+                
+                
+            }
+//
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+//                print("Data: \(utf8Text)") // original server data as UTF8 string
+//                let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary
+//                let json = try! JSONSerialization.jsonObject(with: data, options:[])
+//                print(json["query"] ?? "だめだ")
+            }
+            
+            if let dict = response.result.value as? NSDictionary {
+//                print(dict)
+//                print(dict["query"])
+                guard let query = dict["query"] as? NSDictionary else {
+                    return
+                }
+                
+                guard let results = query["results"] as? NSDictionary else {
+                    return
+                }
+                
+                guard let items = results["item"] as? NSArray else {
+                    return
+                }
+                items.forEach {
+                    print(($0 as! NSDictionary)["title"] as! String)
+                }
+            }
+        }
+    }
+    
+    func loadXML() {
+        
+        let url_text = "http://soccerlture.com/feed/"
+        
+        guard let url = URL(string: url_text) else{
+            return
+        }
+        // インターネット上のXMLを取得し、NSXMLParserに読み込む
+        guard let parser = XMLParser(contentsOf: url) else{
+            return
+        }
+        parser.delegate = self;
+        parser.parse()
+    }
+    
+    // XML解析開始時に実行されるメソッド
+    func parserDidStartDocument(_ parser: XMLParser) {
+        print("XML解析開始しました")
+    }
+    
+    // 解析中に要素の開始タグがあったときに実行されるメソッド
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        print("開始タグ:" + elementName)
+    }
+    
+    // 開始タグと終了タグでくくられたデータがあったときに実行されるメソッド
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        print("要素:" + string)
+    }
+    
+    // 解析中に要素の終了タグがあったときに実行されるメソッド
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        print("終了タグ:" + elementName)
+    }
+    
+    // XML解析終了時に実行されるメソッド
+    func parserDidEndDocument(_ parser: XMLParser) {
+        print("XML解析終了しました")
+    }
+    
+    // 解析中にエラーが発生した時に実行されるメソッド
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        print("エラー:" + parseError.localizedDescription)
+    }
 }
 
---
+++ RSSTEST/ViewController1.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController1.swift
+//  RSSTEST
+//
+//  Created by DaichiSaito on 2018/01/27.
+//  Copyright © 2018年 Orfool inc. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SafariServices
+class ViewController1: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var array = [Any]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+
+//        fetchRSS(url: "http://golf1000.blog23.fc2.com/?xml")
+        fetchRSS(url: "http://www.analyze2005.com/mkblogneo/?feed=rss2")
+//        fetchRSS(url: "http://blog.secret-golf.com/index.rdf")
+//        fetchRSS(url: "http://rssblog.ameba.jp/crenshaw2/rss20.xml")
+//        fetchRSS(url: "http://rssblog.ameba.jp/50shoulder/rss20.xml")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func fetchRSS(url: String) {
+        let parameters: Parameters = ["q": "select * from rss where url = '"+url+"'", "format": "json"]
+        Alamofire.request("https://query.yahooapis.com/v1/public/yql", parameters: parameters).responseJSON { response in
+//            print("Request: \(String(describing: response.request))")   // original url request
+//            print("Response: \(String(describing: response.response))") // http url response
+//            print("Result: \(response.result)")                         // response serialization result
+            
+            
+            if let dict = response.result.value as? NSDictionary {
+                
+                guard let query = dict["query"] as? NSDictionary else {
+                    return
+                }
+                
+                guard let results = query["results"] as? NSDictionary else {
+                    return
+                }
+                
+                guard let items = results["item"] as? NSArray else {
+                    return
+                }
+                items.forEach {
+//                    print(($0 as! NSDictionary)["title"] as! String)
+                    print(($0 as! NSDictionary))
+                    self.array.append($0)
+                }
+                
+//                self.array = items
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+}
+
+extension ViewController1: UITableViewDataSource {
+    //各セルの要素を設定する
+    func tableView(_ table: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = table.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        // Tag番号 ２ で UILabel インスタンスの生成
+        let label = cell.viewWithTag(1) as! UILabel
+        let title = (self.array[indexPath.row] as! NSDictionary)["title"] as! String
+        label.text = String(describing: title)
+        return cell
+    }
+    
+    // Section数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Table Viewのセルの数を指定
+    func tableView(_ table: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.array.count
+    }
+}
+
+extension ViewController1: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let url = (self.array[indexPath.row] as! NSDictionary)["link"] as! String
+        let transformedURL = URL(string: url)
+        let safariViewController = SFSafariViewController(url: transformedURL!)
+        
+        present(safariViewController, animated: true, completion: nil)
+    }
+}
